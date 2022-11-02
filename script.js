492191804c75b69080fe3f5dd107ab0ac21230f8
Bobby <bobbyrayrod92@gmail.com>
2022-11-02 17:51:23 -0600

    worked on the prompts for the password criteria. set characters for Uppercase, Lowercase, special, and numbers

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var choiceArr = [];
 var specialCharacterArr = ["!", "@", "#", "$", "%", "?", "/",];
 var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numberArr = [];
+var numberArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -28,3 +28,35 @@ function generatePassword() {
 
 
 }
+
+function getPrompts(){
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("character length has to be a number, 8 - 128 digits. Please try again.");
+    return false;
+  }
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharacterArr);
+  }
+
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+
+  return true;
+
+
+
+
+
+}
+
