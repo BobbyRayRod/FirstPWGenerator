3d99760c9e4632cb7acb7e190583ae271c40709c
Bobby <bobbyrayrod92@gmail.com>
2022-11-01 22:26:12 -0600

    added multiple VAR's for the lowercase, uppercase, and others

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // Assignment code here
 
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharacterArr = ["!", "@", "#", "$", "%", "?", "/",];
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberArr = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,5 +24,7 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+
+
+}
