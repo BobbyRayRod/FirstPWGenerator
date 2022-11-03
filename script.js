08a2bca03d4789d5e0ff51619fdf886025bba8df
Bobby <bobbyrayrod92@gmail.com>
2022-11-02 18:48:32 -0600

    Thought i had it workng, then it wasnt

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 
 var characterLength = 8;
 var choiceArr = [];
@@ -8,7 +7,7 @@ var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
-// Get references to the #generate element
+// Assignment code here
 var generateBtn = document.querySelector("#generate");
 
 
@@ -17,19 +16,28 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var correctPrompts = getPrompts();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    if(correctPrompts) {
+      var newPassword = generatePassword();
+      passwordText.value = newPassword;
 
 }
 
 function generatePassword() {
-
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomLetter = Math.floor(Math.random() = choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
 
 }
 
 function getPrompts(){
+  choiceArr = [];
+
   characterLength = parseInt(prompt("How many characters do you want your password to be? (8 - 128 characters"));
 
   if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
@@ -51,12 +59,6 @@ function getPrompts(){
   if (confirm("Would you like numbers in your password?")) {
     choiceArr = choiceArr.concat(numberArr);
   }
-
+  
   return true;
-
-
-
-
-
 }
-
